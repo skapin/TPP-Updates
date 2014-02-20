f5f3091e81cd6c417d1c7f0e34991a0a5d3ad355
Drew DeVault <me@drewdevault.com>
2014-02-20 10:34:41 -0800

    Everything else

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
-var tpp_start_time = 1392254634220;
-var update_interval = 3000;
-var api_version = 1;
+var tpp_start_time = 1392254507;
+var update_interval = 5000;
+var api_version = 9;
 
 function ViewModel() {
     var self = this;
 
-    self.filter = ko.observable(['Travel', 'Roster', 'Inventory', 'Battles', 'Meta']);
+    self.filter_options = ko.observable(['Travel', 'Roster', 'Items', 'Battle', 'Commentary', 'Strategy']);
+    self.filter = ko.observableArray();
+    for (var i = 0; i < self.filter_options().length; i++) {
+        self.filter.push(self.filter_options()[i]);
+    }
     self.error = ko.observable(null);
     self.updates = ko.observableArray();
+    self.inventory = ko.observableArray();
+    self.balance = ko.observable(0);
+    self.party = ko.observableArray();
+    self.goal = ko.observable();
+    self.badges = ko.observableArray();
+    self.time = ko.observable("time");
+    self.mapURL = ko.observable("https://pokeworld.herokuapp.com/rb/1");
+
+    self.icon = function(mode) {
+        return '/images/anarchy.png';
+    }
 
     self.filtered = ko.computed(function() {
         var result = [];
         var updates = self.updates();
         for (var i = 0; i < updates.length; i++) {
-            if (self.filter().indexOf(updates[i].category) !== -1) {
+            if (self.filter().indexOf(updates[i].category) !== -1 || updates[i].category === 'Meta') {
                 result.push(updates[i]);
             }
         }
@@ -24,28 +39,88 @@ function ViewModel() {
         return self.filtered().slice(0, 5);
     }, self);
 
+    self.first = ko.computed(function() {
+        return self.filtered()[0];
+    }, self);
+
+    self.recentishUpdates = ko.computed(function() {
+        return self.filtered().slice(1, 7);
+    }, self);
+
     self.oldUpdates = ko.computed(function() {
         return self.filtered().slice(5);
     }, self);
 
+    self.oldishUpdates = ko.computed(function() {
+        return self.filtered().slice(7);
+    }, self);
+
+    self.updateFilter = function(data, e) {
+        if (self.filter().indexOf(data) === -1) {
+            self.filter.push(data);
+            e.target.classList.remove('disabled');
+        } else {
+            self.filter.remove(data);
+            e.target.classList.add('disabled');
+        }
+    };
+
+    self.timeConvert = function(timestamp) {
+        var seconds_per_day = 60 * 60 * 24;
+        var time = timestamp - tpp_start_time;
+        var days = Math.floor(time / seconds_per_day);
+        var remainder = Math.floor(time % seconds_per_day);
+        var hours = Math.floor(remainder / (60 * 60));
+        var remainder = Math.floor(remainder % (60 * 60));
+        var minutes = Math.floor(remainder / 60);
+        var seconds = Math.floor(remainder % 60);
+        return days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+    };
+
     var getUpdates = function() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/feed.json');
-        xhr.onload = function() {
-            try {
-                var json = JSON.parse(this.responseText);
-                if (json.version > api_version) {
-                    window.location = window.location;
+        try
+        {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/feed.json?i=' + new Date().getTime());
+            xhr.onload = function() {
+                try {
+                    var json = JSON.parse(this.responseText);
+                    if (json.version > api_version) {
+                        window.location = window.location;
+                    }
+                    self.updates(json.updates);
+                    self.inventory(json.inventory);
+                    self.balance(json.balance);
+                    self.party(json.party);
+                    self.goal(json.goal);
+                    self.badges(json.badges);
+                    if (self.mapURL() != json.mapURL) {
+                        self.mapURL(json.mapURL);
+                        var map = document.getElementById('map');
+                        if (map) {
+                            map.src = map.src;
+                        }
+                    }
+                } catch (e) {
+                    self.error("Error getting latest news.");
+                } finally {
+                    setTimeout(getUpdates, update_interval);
                 }
-                self.updates(json.updates);
-            } catch (e) {
-                self.error("Error getting latest news.");
-            }
-        };
-        xhr.send();
+            };
+            xhr.send();
+        } catch (ex) {
+            console.log("Error updating: " + ex);
+            setTimeout(getUpdates, update_interval);
+        }
     };
     getUpdates();
-    setInterval(getUpdates, update_interval);
+
+    var updateTime = function() {
+        var date = new Date();
+        self.time("Your UTC: " + ("0" + date.getMinutes()).slice(-2) + " :" + ("0" + date.getSeconds()).slice(-2) + " ." + ("0" + Math.floor(date.getMilliseconds() / 10)).slice(-2));
+        setTimeout(updateTime, 50);
+    };
+    updateTime();
     
     return self;
 };
